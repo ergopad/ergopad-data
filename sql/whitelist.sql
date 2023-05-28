f5dc428a798dd0050fdf643650358e55164456d1
noob77777 <42897033+noob77777@users.noreply.github.com>
2023-05-28 10:45:42 +0530

    :update: whitelist.sql

--- sql/whitelist.sql
+++ sql/whitelist.sql
@@ -2,7 +2,7 @@ DROP TABLE "whitelist";
 -- whitelist
 CREATE TABLE "whitelist" (
     id SERIAL PRIMARY KEY,
-    "walletId" INTEGER NOT NULL,
+    "walletId" INTEGER DEFAULT -1,
     "eventId" INTEGER DEFAULT -1,
     "created_dtz" timestamptz NOT NULL DEFAULT now(),
     "allowance_sigusd" NUMERIC(16, 6) DEFAULT 0.0,
